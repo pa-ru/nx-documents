e18609584bbba3d096f929990199405e9ca04e67
pa-ru <patrick.rupp@gmx.de>
2019-11-02 21:46:26 +0100

    add multipart upload to documents api

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,14 +1,21 @@
 import { Document } from '@nx-document/model';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get('documents')
-  getData() : Document[]{
+  getData(): Document[] {
     return this.appService.getDocuments();
+  }
+
+  @Post('documents')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file) {
+    console.log(file);
   }
 }
