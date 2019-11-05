a04669d546a4a2110b6555f68fcdf564458e264c
pa-ru <patrick.rupp@gmx.de>
2019-11-05 13:07:53 +0100

    use cdk virtual scrolling

--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
@@ -10,7 +11,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { ViewerComponent } from './viewer/viewer.component';
 
 @NgModule({
-  declarations: [AppComponent, UploadComponent, ViewerComponent],
+  declarations: [
+    AppComponent,
+    UploadComponent,
+    ViewerComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
@@ -19,7 +23,8 @@ import { ViewerComponent } from './viewer/viewer.component';
       url: 'http://localhost:3333',
       options: {},
     }),
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ScrollingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
